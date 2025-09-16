bdff1fdfad159ae0a07129447cf8624db99b7533
noeh0703 <noheliatinco@gmail.com>
2025-09-15 21:22:59 -0500

    Modificaciones-datos

--- src/Utilidades/InicializadorDatos.java
+++ src/Utilidades/InicializadorDatos.java
@@ -2,6 +2,10 @@
 package Utilidades;
 
 
-public class InicializadorDatos {
-    
+public class InicializadorDatos 
+{
+    public static void inicializarArchivos(String rutaCarpeta){
+        crearArchivoConDatos(rutaCarpeta, "usuario.txt",  "12345678,Juan Perez,987654321\n" +
+                "87654321,Ana Torres,912345678\n");
+    }
 }
