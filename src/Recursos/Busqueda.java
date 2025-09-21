a87ed7016f232bd080220f9dbf0257f250d73110
noeh0703 <noheliatinco@gmail.com>
2025-09-20 21:18:42 -0500

    Busqueda(interna)

--- src/Recursos/Busqueda.java
+++ src/Recursos/Busqueda.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 public class Busqueda {
     //BUSQUEDA SECUENCIAL (memoria) interna
-    
+    public static Libro busquedaSecuencial(ArrayList<Libro> libros, String tituloBuscado){
+        for(Libro libro : libros){
+            if(libro.getTitulo().equalsIgnoreCase(tituloBuscado)){
+                return libro; //lo encontro
+            }
+        }
+        return null;//no lo encontro
+    }
+    // BUSQUEDA HASH (memori interna)
     
 }
