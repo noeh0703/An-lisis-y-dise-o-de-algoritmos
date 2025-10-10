680bce634c0bf99a49f0c771936543245e77451c
noeh0703 <noheliatinco@gmail.com>
2025-10-09 22:23:17 -0500

    Busqueeda

--- src/Recursos/Busqueda.java
+++ src/Recursos/Busqueda.java
@@ -43,5 +43,26 @@ public class Busqueda {
         }
         return null; // no encontrado
     }
+     
+    // BÚSQUEDA BINARIA EXTERNA (archivo debe estar ORDENADO por título)
+    public static String busquedaBinariaExterna(String archivo, String tituloBuscado) {
+        List<String> lineas = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+            return null;
+        }
         
-}
+        
+        
+        
+        
+        
+        
+       }
+    }
+
