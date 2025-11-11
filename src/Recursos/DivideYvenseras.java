d9104fc64fbc49b2ffd39f87bbc69d02d62ff8f5
noeh0703 <noheliatinco@gmail.com>
2025-11-10 20:53:55 -0500

    quickSort

--- src/Recursos/DivideYvenseras.java
+++ src/Recursos/DivideYvenseras.java
@@ -4,5 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class DivideYvenseras {
     //QUICK-SORT ordenr por titulo
-    
+    public static void quickSortLibros(ArrayList<Libro> libros, int inicio, int fin){
+        if(inicio < fin){
+            int indicePivote = particion(libros, inicio, fin);
+            //lamada recursiva para ordenar las sublistas
+            quickSortLibros(libros, inicio, indicePivote -1);
+            quickSortLibros(libros, indicePivote + 1, fin);
+        }
+    }
 }
